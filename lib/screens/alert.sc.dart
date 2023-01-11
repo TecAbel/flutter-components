6381157d20549633d8b89ba771a40221bfa56047
TecAbel <abelardo96.work@gmail.com>
2023-01-10 22:05:06 -0600

    feat(alert-sc): added alert dialog

--- lib/screens/alert.sc.dart
+++ lib/screens/alert.sc.dart
@@ -3,8 +3,41 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
-  void showAlertDialog() {
-    print('hola');
+  void showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        title: const Text('Esto es una alerta'),
+        icon: const Icon(
+          Icons.warning,
+          color: Colors.red,
+        ),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Este es el contenido de la alerta'),
+            const SizedBox(
+              height: 20,
+            ),
+            Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cerrar'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      barrierDismissible: true,
+    );
   }
 
   @override
@@ -21,7 +54,7 @@ class AlertScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: showAlertDialog,
+              onPressed: () => showAlertDialog(context),
               child: const Padding(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text('Mostar alerta'),
