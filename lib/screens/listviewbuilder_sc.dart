4fb14e6ffd70c9b45efe8437cfdc898b2926b660
TecAbel <abelardo96.work@gmail.com>
2023-01-16 15:07:31 -0600

    feat(listviewbuilder): add 5 more items function

--- lib/screens/listviewbuilder_sc.dart
+++ lib/screens/listviewbuilder_sc.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({super.key});
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imageItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  void add5() {
+    final last = imageItems.last;
+    imageItems.addAll([1, 2, 3, 4, 5].map((i) => last + i));
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      /* print(
+          '${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}'); */
+      if ((scrollController.position.pixels + 300) >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +38,9 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          itemCount: imageItems.length,
+          controller: scrollController,
           itemBuilder: ((context, index) {
             return FadeInImage(
               width: double.infinity,
@@ -19,7 +48,7 @@ class ListViewBuilderScreen extends StatelessWidget {
               fit: BoxFit.cover,
               placeholder: const AssetImage('assets/jar-loading.gif'),
               image: NetworkImage(
-                  'https://picsum.photos/500/300?image=${index + 1}'),
+                  'https://picsum.photos/500/300?image=${imageItems[index]}'),
             );
           }),
         ),
